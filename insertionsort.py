9f162d85feff4d5fd2754cda922b38538749adaf
Pavani <pchavali39711@ucumberlands.edu>
2025-10-26 13:28:49 -0700

    insertionsorting

---
+++ insertionsort.py
@@ -0,0 +1,18 @@
+example = [4,3,6,2,7,1]
+
+for i in range(1,len(example)):
+    curr = example[i]
+    prev =i-1
+    while prev>=0 and example[prev]<curr:
+        example[prev+1] = example[prev]
+        prev = prev-1   
+    example[prev+1] = curr
+print(example)
+
+
+    
+        
+        
+        
+        
+
